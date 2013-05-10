e5a8895d8272c655a5ca43df451475bab66c0ca6
Arata Tanaka <t.arata.43@gmail.com>
2013-05-11 01:30:49 +0900

    サンプルユーザの追加

--- fuel/app/tasks/create.php
+++ fuel/app/tasks/create.php
@@ -45,5 +45,68 @@ class Create
             $seminar->save();
 	}
 
+        public static function sample_user()
+        {
+            $user   = \Model_User::forge();
+            $auth   = \Auth::instance();
+            $already    = \Model_User::find(array('username'=>'arata'));
+            if($already) exit;
+            $user->username     = 'arata';
+            $user->email    = 'tarata43@yahoo.co.jp';
+            $user->password = $auth->hash_password('arataarata');
+            $user->save();
+        }
+
+        public static function sample()
+        {
+            $sample_arr = array(
+                array(
+                    'id'    => 1,
+                    'title' => '集合知プログラミング読書会',
+                    'date'  => '2013-04-06',
+                    'requirements'  => '',
+                    'notes' => '',
+                    'slides'=> 'https://docs.google.com/presentation/d/1Q5FJWcymdwalFfhYoIEHFo1565rjLQKm6Ob0OGjbDFI/edit?usp=sharing',
+                    'references'    => '集合知プログラミング',
+                ),
+                array(
+                    'id'    => 2,
+                    'title' => 'フレームワークに触ろう - fuelphp padrino',
+                    'date'  => '2013-04-20',
+                    'requirements'  => '',
+                    'notes' => '',
+                    'slides'=> 'https://docs.google.com/presentation/d/1pROnetghdwS9bz6K58OG11jMmAIOXcVMYbnC_mGaK-Q/edit?usp=sharing',
+                    'references'    => '',
+                ),
+                array(
+                    'id'    => 3,
+                    'title' => 'Engineerでも知っておきたいDesignの基本',
+                    'date'  => '2013-04-27',
+                    'requirements'  => '',
+                    'notes' => '',
+                    'slides'=> 'https://docs.google.com/file/d/0B3gddgi1VarzVnRGMXZqd0J3YVU/edit?usp=sharing',
+                    'references'    => '',
+                ),
+                array(
+                    'id'    => 4,
+                    'title' => '分散処理入門',
+                    'date'  => '2013-05-04',
+                    'requirements'  => '',
+                    'notes' => '',
+                    'slides'=> 'http://www.slideshare.net/ryukln/hadoopstartup',
+                    'references'    => '「分散システム ~原理とパラダイム~」タネンバウム'.PHP_EOL.'http://www.amazon.co.jp/分散システム-第二版-アンドリュー・S・タネンバウム/dp/4894714981'.PHP_EOL.'「Googleを支える技術」西田圭介'.PHP_EOL.'http://www.amazon.co.jp/exec/obidos/ASIN/4774134325/secodezine-22/'.PHP_EOL.'「はじめてのHadoop」 田澤孝之、横井浩、松井一比良'.PHP_EOL.'http://gihyo.jp/book/2012/978-4-7741-5389-6'.PHP_EOL.'阪大の講義資料'.PHP_EOL.'http://www-higashi.ist.osaka-u.ac.jp/~nakata/mobile-cp/chap-01j.pdf',
+                ),
+            );
+            foreach($sample_arr as $sample)
+            {
+                $seminar    = \Model_Seminar::find($sample['id']);
+                if(!$seminar)$seminar    = \Model_Seminar::forge();
+                unset($sample['id']);
+                $seminar->set($sample);
+                $seminar->save();
+            }
+
+        }
+
 }
 /* End of file tasks/create.php */
