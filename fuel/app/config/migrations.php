518d11362ff74f3a9b2950f5cd81648f0fe5d413
Arata Tanaka <t.arata.43@gmail.com>
2013-05-11 03:44:27 +0900

    本番環境用の修正

---
+++ fuel/app/config/migrations.php
@@ -0,0 +1,26 @@
+<?php
+return array(
+	'version' => 
+	array(
+		'app' => 
+		array(
+			'default' => 
+			array(
+				0 => '001_create_users',
+				1 => '002_add_email_to_users',
+				2 => '003_add_last_login_to_users',
+				3 => '004_add_login_hash_to_users',
+				4 => '005_create_seminars',
+				5 => '006_create_comments',
+			),
+		),
+		'module' => 
+		array(
+		),
+		'package' => 
+		array(
+		),
+	),
+	'folder' => 'migrations/',
+	'table' => 'migration',
+);
